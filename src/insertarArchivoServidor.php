f2a1faf19162fb8cabd6ca17f0b3ce941268b9e7
lara anabel <55300998+glaraanabelperez@users.noreply.github.com>
2021-04-13 23:02:37 -0300

    Update insertarArchivoServidor.php

--- src/insertarArchivoServidor.php
+++ src/insertarArchivoServidor.php
@@ -1,22 +1,36 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-    
-    var_dump($_FILES);
-    $nombre_img=$_FILES['file']['name'];
-    $ruta_temp_actual=$_FILES['file']['tmp_name'];  
-    $ruta_destino=dirname(__FILE__)."\\assets\\".$nombre_img;
-    // $ruta_destino=dirname(__FILE__)."\\assets\\".$nombre_img;
-    $rsta=move_uploaded_file($ruta_temp_actual, $ruta_destino);
-    class Result{}
-    $response=new Result();
-    if ($rsta) {
-      $response->resultado='OK';
-
-     }else{
-         $response->resultado='Error';
-        }
-
-    header('Content-Type: application/json');
-    echo json_encode($response);//muestra el json , el mensaje 
+    if(isset($_FILES['file'])){
+		$imagen_nombre = $_FILES['file']['name'];
+		$directorio_final = "./assets/".$imagen_nombre; 
+			if(move_uploaded_file($_FILES['file']['tmp_name'], $directorio_final)){
+				$data = array(
+					'status' => 'success',
+					'code' => 200,
+					'msj' => 'Imagen subida'
+				);
+				$format = (object) $data;
+				$json = json_encode($format); 
+				echo $json; 
+			}else{
+				$data = array(
+					'status' => 'error',
+					'code' => 400,
+					'msj' => 'Error al mover imagen al servidor'
+				);
+				$format = (object) $data;
+				$json = json_encode($format); 
+				echo $json; 
+			}
+	}else{
+		$data = array(
+			'status' => 'error',
+			'code' => 400,
+			'msj' => 'No se recibio ninguna imagen'
+		);
+		$format = (object) $data;
+		$json = json_encode($format); 
+		echo $json; 
+	}
 ?>
