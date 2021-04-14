429cd7f83b31e245c41ab03a2906b2e75147366a
lara anabel <55300998+glaraanabelperez@users.noreply.github.com>
2021-04-13 23:03:36 -0300

    Update borrarArchivoServidor.php

--- src/borrarArchivoServidor.php
+++ src/borrarArchivoServidor.php
@@ -1,22 +1,40 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     header("Access-Control_Allow-Headers: origin, X-Requested-With,Content-Type, Accept");//le pide acceso al header
-    header('Access-Control-Allow-Methods: GET, POST');
-    header("Accept:application/json, text/plain, */*");
+    // header('Access-Control-Allow-Methods: GET, POST');
+    // header("Accept:application/json, text/plain, */*");
+
     $json = file_get_contents('php://input');
     $obj = json_decode($json);
     $nombre_fichero=dirname(__FILE__)."\\assets\\".$obj;
-    $rtsa=unlink($nombre_fichero);
+  
+      if(isset($nombre_fichero)){
+        $rtsa=unlink($nombre_fichero);
 
-    class Result{}
-      $response=new Result();
-      if ($rsta) {
-          $response->resultado='OK';
-         }else{
-             $response->resultado='Error';
-            }
-
-        header('Content-Type: application/json');
-        echo json_encode($response);//muestra el json , el mensaje
-        
+			if(isset($rsta)){
+				$data = array(
+					'status' => 'success',
+					'msj' => 'Imagen borrada.'
+				);
+				$format = (object) $data;
+				$json = json_encode($format); 
+				echo $json; 
+			}else{
+				$data = array(
+					'status' => 'error',
+					'msj' => 'Error al borrar imagen.'
+				);
+				$format = (object) $data;
+				$json = json_encode($format); 
+				echo $json; 
+			}
+	}else{
+		$data = array(
+			'status' => 'error',
+			'msj' => 'No se recibio ninguna imagen'
+		);
+		$format = (object) $data;
+		$json = json_encode($format); 
+		echo $json; 
+	}
 ?>
